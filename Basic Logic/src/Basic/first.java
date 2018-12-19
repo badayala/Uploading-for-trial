d16cf4be58561b33449f1a8874158e44139d9223
rahul <rahulbadayala@gmail.com>
2018-12-19 23:56:22 +0530

    first commit

---
+++ Basic Logic/src/Basic/first.java
@@ -0,0 +1,35 @@
+package Basic;
+public class first {
+	
+	public static void main(String[] args) {
+		/*{
+			int i = 1;
+			while(i<=10)
+			  {
+				System.out.println("Hello My friends");
+				i++;
+				
+			  }
+			
+		}*/
+		
+	
+	
+	 //TODO Auto-generated method stub
+		int sum = 0;
+		   
+	      for (int i = 0; i <= 1000; i++) {
+	    	  sum = sum + i;     // sum = sum + i
+	      	}
+	      		{													
+	      			System.out.println("Sum = " + sum);  
+	      		}
+	      
+			}
+	}
+	/*for (int i = 1; i <= 10; ++i) {
+    System.out.println("Line " + i);
+ */
+	
+
+
